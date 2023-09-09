3ebc9a3d1bd2e34bc72e9404d9dae3bdd5923791
savoygu <1029559886@qq.com>
2023-09-09 10:32:40 +0800

    Update WebStacks type and function names

--- scripts/src/main.ts
+++ scripts/src/main.ts
@@ -2,7 +2,7 @@ import { resolve } from 'node:path'
 import { readFileSync, writeFile, writeFileSync } from 'node:fs'
 import { defineCommand, runMain } from 'citty'
 import yaml from 'js-yaml'
-import type { Link, List, WebStacks } from './types'
+import type { NestWebStack, WebStack, WebStacks } from './types'
 
 export const main = defineCommand({
   meta: {
@@ -16,19 +16,19 @@ export const main = defineCommand({
 
     const webstacks = yaml.load(readFileSync(input, 'utf-8')) as WebStacks
     let readme = readFileSync(output, 'utf-8')
-    readme = replaceContent(readme, transformWebStacks(webstacks), 'WEB_STACK')
+    readme = fillPlaceholder(readme, transformWebStacks(webstacks), 'WEB_STACK')
     writeFileSync(output, readme, 'utf-8')
   },
 })
 
 runMain(main)
 
-function replaceContent(fileContent: string, replacedContent: string, placeholder: string, insertionPosition: 'head' | 'tail' | 'none' = 'none') {
+function fillPlaceholder(fileContent: string, filledContent: string, placeholder: string, insertionPosition: 'head' | 'tail' | 'none' = 'none') {
   const START = `<!--${placeholder}_STARTS-->`
   const END = `<!--${placeholder}_ENDS-->`
   const regex = new RegExp(`${START}[\\s\\S]*?${END}`, 'im')
 
-  const target = replacedContent ? `${START}\n${replacedContent}\n${END}` : `${START}${END}`
+  const target = filledContent ? `${START}\n${filledContent}\n${END}` : `${START}${END}`
 
   if (!regex.test(fileContent)) {
     if (insertionPosition === 'none')
@@ -41,11 +41,11 @@ function replaceContent(fileContent: string, replacedContent: string, placeholde
   return fileContent.replace(regex, target)
 }
 
-function transformLinks(links: Link[]) {
+function transformLinks(links: WebStack['links']) {
   return links.map(link => `- <img src="${link.logo}" width="14" height="14" /> [${link.title}](${link.url}) ${link.description}`).join('\n')
 }
 
-function transformList(list: List[]) {
+function transformList(list: NestWebStack['list']) {
   return list.reduce((acc, item) => {
     acc.push(`#### ${item.term}`, transformLinks(item.links))
     return acc
